dceff243a426eeefbfad91b317c712a321861db1
truongvonh <truong.vonh@one-line.com>
2024-05-19 02:35:03 +0700

    update pipelien #20

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello world codepipeline AWS new update pipeline #1';
+    return 'Hello world codepipeline AWS new update pipeline #2';
   }
 }
